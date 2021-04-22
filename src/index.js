edc1e51e6fbeea80b89709f4cfd7f048b6f13cbd
Abdul <sadiqaminu6@gmail.com>
2021-04-22 10:12:51 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {component} from 'react'
-import MyInfo from "./component/MyInfo"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+import MyInfo from "./component/MyInfo";
 
 const newLocal = <MyInfo />;
 ReactDOM.render(
   <React.StrictMode>
-    
-    <App />{newLocal}
+    <App />
+    {newLocal}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
