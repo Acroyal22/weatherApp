d045662609437f760ad68778f5efa60421076e8a
Abdul <sadiqaminu6@gmail.com>
2021-04-22 09:54:00 +0100

    editting was made

--- src/component/Keys.js
+++ src/component/Keys.js
@@ -1,7 +1,4 @@
 module.exports = {
-            API_KEY: "1e7d4807f020594ccd46856d42447853",
-            BASE_URL: "https://api.openweathermap.org/data/2.5/",
-          };
-    
-
-        
+  API_KEY: "8c703a22fbe0b30078425caa0a3977da",
+  BASE_URL: "https://api.openweathermap.org/data/2.5/",
+};
--- src/component/MyInfo.js
+++ src/component/MyInfo.js
@@ -1,37 +1,41 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import Keys from "./Keys";
-import Cards from "./Cards";
+import axios from "axios";
 
 const api = {
   key: Keys.API_KEY,
   base: Keys.BASE_URL,
 };
-const dateBuild = (date) => {
-  let time = String(new window.Date());
-   time = Date.slice(3, 15);
-  return time;
-};
+
+const dateString = Date().toString();
 
 function MyInfo() {
-  const [query, setQuerry] = useState("");
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
+  const [errMessage, setErrMessage] = useState("");
 
   const search = (event) => {
     event.preventDefault();
+    try {
+      axios
+        .get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((response) => {
+          setWeather(response.data);
+        })
+        .catch((error) => {
+          setErrMessage(error);
+        });
+    } catch (err) {
+      console.log(err);
+      console.log(err.response.data.message);
 
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then((response) => response.json())
-      .then((result) => {
-        setQuerry("");
-        setWeather(result);
-        console.log(result);
-      });
+      setErrMessage(err.response.data.message);
+    }
+
+    console.log(weather.main);
   };
-if (weather.cod===405) {
-  alert("WRONG STATE")
-}
+
   return (
-  
     <div
       className={
         typeof weather.main != "undefined"
@@ -41,39 +45,43 @@ if (weather.cod===405) {
           : "App"
       }
     >
-      
       <main>
         <form className="search-container" onSubmit={search}>
           <input
             type="text"
             placeholder="Search..."
             className="search-bar"
-            onChange={(event) => setQuerry(event.target.value)}
+            onChange={(event) => setQuery(event.target.value)}
             value={query}
-          
           />
           <input type="submit" className="search" />
-
         </form>
 
-        {typeof weather.main != "undefined" ? (
+        {typeof weather.main !== "undefined" ? (
           <div>
             <div className="location-container">
               <div className="location">
-                {weather.name}, {weather.sys.country}
+                {weather.name}, {weather?.sys?.country}
               </div>
             </div>
             <div className="weather-container">
               <div className="temperature">
                 {Math.round(weather.main.temp)}°C
               </div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather">{weather.weather.main}</div>
             </div>
-            <div className="date">{dateBuild}</div>
+            <div className="date">Current Time :{dateString}</div>
           </div>
         ) : (
-          ""
+          "this is working"
         )}
+        <div className="error">
+          {errMessage.length > 0 ? (
+            <span style={{ color: "red" }} className="text-danger">
+              {errMessage}
+            </span>
+          ) : null}
+        </div>
       </main>
     </div>
   );
